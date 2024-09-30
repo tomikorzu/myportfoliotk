5c6c802ca4b9f292504a9034070a9c456fda0181
tomikorzu <tomykorzu@icloud.com>
2024-09-30 20:37:57 -0300

    feat(pages): add all the component pages that i will use

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home.jsx";
+import About from "./pages/About.jsx";
+import Projects from "./pages/Projects.jsx";
+import Contact from "./pages/Contact.jsx";
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </Router>
   );
---
+++ src/pages/Projects.jsx
@@ -0,0 +1,8 @@
+const Projects = () => {
+  return (
+    <>
+      <h1>Projects</h1>
+    </>
+  );
+};
+export default Projects;
