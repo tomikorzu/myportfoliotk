30fd752466dc1185887fa259425eec979b241f54
tomikorzu <tomykorzu@icloud.com>
2024-10-08 00:30:10 -0300

    feat(about.jsx): connect the textAboutMe in About page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,6 +1,14 @@
+import { useEffect } from "react";
 import NavbarMenu from "../components/NavbarMenu";
+import { allData } from "../utils/variables.js";
 
 const About = () => {
+  useEffect(() => {
+    const aboutMe = allData[0];
+    document.getElementById("about-me-text").textContent = aboutMe.aboutMe;
+    document.getElementById("objective-professional-text").textContent =
+      aboutMe.professionalObjective;
+  }, []);
   return (
     <>
       <div className="back-drop fade-in">
@@ -13,26 +21,13 @@ const About = () => {
           ]}
         />
         <h1>About Me</h1>
-        <div className="about-container">
-          <div className="about-left">
-            <h2>Tomás Korzusehec</h2>
-            <p>
-              I am a Front End Developer with a passion for creating beautiful
-              and functional websites. I have experience with HTML, CSS,
-              JavaScript, React, and Node.js. I am always looking to learn new
-              things and improve my skills.
-            </p>
-          </div>
-          <div className="about-right">
-            <h3>Skills</h3>
-            <ul>
-              <li>HTML</li>
-              <li>CSS</li>
-              <li>JavaScript</li>
-              <li>React</li>
-              <li>Node.js</li>
-            </ul>
-          </div>
+        <div>
+          <h3>How I am</h3>
+          <h5 id="about-me-text"></h5>
+        </div>
+        <div>
+          <h3>My Professional objective</h3>
+          <h6 id="objective-professional-text"></h6>
         </div>
       </div>
     </>
