1fdb8e012812a9f701456b48cfb14b5eb1445606
tomikorzu <tomykorzu@icloud.com>
2024-10-07 22:01:29 -0300

    feat(variables.js): add all about me and projects data

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,42 @@
+import NavbarMenu from "../components/NavbarMenu";
+
 const About = () => {
-    return (
-        <>
-            <h1>About</h1>
-        </>
-    )
-}
-export default About
+  return (
+    <>
+      <div className="back-drop fade-in">
+        <NavbarMenu
+          items={[
+            { item: "Home", url: "/" },
+            { item: "About", url: "/about", active: true },
+            { item: "Projects", url: "/projects" },
+            { item: "Contact", url: "/contact" },
+          ]}
+        />
+        <h1>About Me</h1>
+        <div className="about-container">
+          <div className="about-left">
+            <h2>Tomás Korzusehec</h2>
+            <p>
+              I am a Front End Developer with a passion for creating beautiful
+              and functional websites. I have experience with HTML, CSS,
+              JavaScript, React, and Node.js. I am always looking to learn new
+              things and improve my skills.
+            </p>
+          </div>
+          <div className="about-right">
+            <h3>Skills</h3>
+            <ul>
+              <li>HTML</li>
+              <li>CSS</li>
+              <li>JavaScript</li>
+              <li>React</li>
+              <li>Node.js</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default About;
---
+++ src/utils/variables.js
@@ -0,0 +1,125 @@
+const skillsSet = {
+  languages: [
+    "HTML",
+    "CSS",
+    "JavaScript",
+    "TypeScript",
+    "MySQL",
+    "Python",
+    "Java",
+    "PHP",
+  ],
+  libraries: [
+    "React",
+    "Redux",
+    "Axios",
+    "Framer Motion",
+    "Styled Components",
+    "JQuery",
+    "Lodash",
+    "Three.js",
+  ],
+  frameworks: [
+    "Next.js",
+    "Express",
+    "Node.js",
+    "Vue.js",
+    "Angular",
+    "Django",
+    "Laravel",
+    "Nest.js",
+    "Svelte",
+  ],
+  tools: [
+    "Git",
+    "GitHub",
+    "VS Code",
+    "Figma",
+    "Slack",
+    "Trello",
+    "Notion",
+    "Docker",
+    "Postman",
+    "Webpack",
+    "Jest",
+    "ESLint",
+    "Prettier",
+    "Discord",
+  ],
+};
+
+const education = [
+  {
+    name: "Huergo",
+    time: "2019 - Present",
+    degree: "Computer Technician",
+    logo: "",
+    description: "lorem",
+    tecnologies: ["HTML", "CSS", "Boostrap", "MySQL", "Python", "Visual Basic"],
+  },
+];
+
+const experience = [
+  {
+    name: "GM2Dev",
+    time: "Jun 2024 - Present",
+    degree: "Front End Developer",
+    logo: "",
+    description: "Lorem",
+    tecnologies: ["HTML", "CSS", "JavaScript", "Git"],
+  },
+];
+
+const links = {
+  cvSpanish: "cvSpanish",
+  cvEnglish: "cvEnglish",
+  linkedin: "https://www.linkedin.com/in/tomas-korzusehec/",
+  github: "https://www.github.com/tomikorzu",
+  mail: "mailto:tomykorzu@icloud.com",
+};
+
+const certifications = [
+  {
+    name: "React",
+    time: "2024",
+    degree: "Front End Developer",
+    logo: "",
+    description: "lorem",
+  },
+];
+
+const projects = [
+  {
+    img: "/",
+    name: "Portfolio",
+    group: "Personal",
+    time: "10 Aug. 2024",
+    description: "lorem",
+    tecnologies: ["HTML", "CSS", "JavaScript", "React"],
+    url: "https://www.google.com",
+    githubRepo: "https://github.com/tomikorzu/repo",
+    vercel: "https://vercel.app",
+  },
+];
+
+const personalData = {
+  info: [
+    { data: "Buenos Aires, Argentina", icon: "fa-solid fa-envelope" },
+    { data: "13/12/2005", icon: "fa-solid fa-cake" },
+    { data: "tomykorzu@icloud.com", icon: "fa-solid fa-envelope" },
+    { data: "+54 9 11 1234-5678", icon: "fa-solid fa-phone" },
+  ],
+  languages: [
+    { language: "Spanish", level: "Native" },
+    { language: "English", level: "Intermediate(B2)" },
+  ],
+};
+export const allData = [
+  skillsSet,
+  education,
+  experience,
+  links,
+  certifications,
+  projects,
+  personalData,
+];
